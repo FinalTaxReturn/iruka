8ac23c86cdff664d62b6734adf8d95bf9d0dff9f
teramon1031 <mio.terakado@styz.io>
2024-03-12 14:11:40 +0900

    hardcoded the frame of home view and made some adjustments

--- app/[roomId]/page.tsx
+++ app/[roomId]/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import UserProfile from "@/components/UserProfile";
+import { accounts } from "./data/accounts";
+import BottomMenuBar from "@/components/BottomBar";
+import { H1, H2, H3 } from "@/components/Headings";
+import TopBar from "@/components/TopBar";
+
+export default async function Page() {
   return (
     <>
-      <h1>Roomのトップ</h1>
+      <TopBar title="Room Name" />
+      <H2>近くのアカウント</H2>
+      {accounts.map((account) => (
+        <UserProfile key={account.id} account={account} />
+      ))}
+      <BottomMenuBar />
     </>
   );
 }
---
+++ components/UserProfile.tsx
@@ -0,0 +1,30 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Card, CardContent, CardHeader } from "./ui/card";
+
+export type Account = {
+  name: string;
+  username: string;
+  avatarUrl: string;
+  following: boolean;
+};
+
+const UserProfile = ({ account }: { account: Account }) => {
+  return (
+    <Card className="my-5">
+      <CardHeader>
+        <div className="flex">
+          <Avatar>
+            <AvatarImage src={account.avatarUrl} />
+            <AvatarFallback>TM</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col ml-2">
+            <span className="font-bold">{account.name}</span>
+            <span className="text-sm text-gray-500">{account.username}</span>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default UserProfile;
