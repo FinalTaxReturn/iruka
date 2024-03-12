c0b3c5879be038aa51fcf6d90a73c581db74f648
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-12 15:53:59 +0900

    Add prettier formatting to lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': [
-      () => 'tsc -p tsconfig.json --noEmit',
-      () => 'next lint',
-    ],
+  '*.{js,jsx,ts,tsx}': [
+    () => 'prettier --write .',
+    () => 'tsc -p tsconfig.json --noEmit',
+    () => 'next lint',
+  ],
 };
