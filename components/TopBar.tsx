94b1f8d3065b1e491ab6417d6afcc18969a011c3
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-16 13:42:34 +0900

    Update TopBar component to add whitespace-nowrap class

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -11,7 +11,7 @@ const TopBar: React.FC<TopBarProps> = ({ title }) => {
     <div className='fixed top-0 left-0 right-0 bg-white shadow-md z-50'>
       <div className='container mx-auto px-5 py-4 grid grid-cols-3 items-center'>
         <div></div>
-        <H1 className='text-center'>{title}</H1>
+        <H1 className='text-center whitespace-nowrap'>{title}</H1>
         <div className='relative w-8 h-8 justify-self-end'></div>
       </div>
     </div>
